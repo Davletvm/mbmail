d7d49aa37c245a2efa3446e03f7d845ccb449680
Davlet <davletvm@gmail.com>
2020-06-21 14:13:59 +0300

    Updates

---
+++ precache-manifest.a6cc2841afdb50ef2aef04495790cda2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a30e9f4b74e1862552632c4a68bf0be",
+    "url": "/mbmail/index.html"
+  },
+  {
+    "revision": "371a2315761dd7fa885f",
+    "url": "/mbmail/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eaabbdfd9b0654197ecd",
+    "url": "/mbmail/static/js/2.f939c634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mbmail/static/js/2.f939c634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371a2315761dd7fa885f",
+    "url": "/mbmail/static/js/main.82c6183e.chunk.js"
+  },
+  {
+    "revision": "d623217b4113915d071a",
+    "url": "/mbmail/static/js/runtime-main.98f5b355.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mbmail/static/media/logo.5d5d9eef.svg"
+  }
+]);
